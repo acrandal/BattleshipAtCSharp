54da5b069af7eea74a9a8717f5de1319a050edad
Aaron S. Crandall <crandall@gonzaga.edu>
2022-02-18 21:24:08 -0800

    Initial project import and setup with NUnit framework in place and Hello World main

---
+++ Battleship/Battleship.cs
@@ -0,0 +1,12 @@
+﻿namespace Battleship;
+public class Battleship
+{
+    public bool IsPrime(int candidate) {
+        throw new NotImplementedException("Please create a test first.");
+    }
+
+    public static void Main(string[] args) {
+        System.Console.WriteLine("Starting Battleship");
+    }
+
+}
